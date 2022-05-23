f3c774c53d944c9a102155cbc592897b94abd5d6
G184252021 <218425@st.takushoku-u.ac.jp>
2022-05-23 23:55:05 +0900

    作成中

--- kazuate.js
+++ kazuate.js
@@ -9,8 +9,6 @@ let kaisu = 0;
 // 将来: ボタンを押したら， hantei() を呼び出すように修正する
 hantei();
 
-let dp = document.querySelector('div#placeholder');
-
 // ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
     let yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
@@ -18,24 +16,39 @@ function hantei() {
     // 課題3-1：ここの判定処理を作成する．
     //        ページに表示する方法はまだ習っていないので
     //        判定結果はコンソールに出力すること
+
+
+    let pp = document.createElement('pp');
+    pp.textContent = kaisu + '回目の予想: ' + yoso;
+    let re = document.querySelector('div#result');
+    re.insertAdjacentElement('beforeend', pp);
     console.log(kaisu + "回目の予想: " + yoso);
+
+    let p = document.createElement('p');
     if (kaisu < 4) {
         if (yoso === kotae) {
-            if (kaisu === 1) {
-                console.log("正解です。おめでとう！");
-            }
-            else {
-                console.log("答えは " + kotae + " でした。すでにゲームは終わっています");
-            }
+            p.textContent = '正解です。おめでとう！';
+            re = document.querySelector('div#result');
+            re.insertAdjacentElement('afterend', p);
+            connsole.log("正解です。おめでとう！");
         }
         else if (kotae < yoso) {
+            p.textContent = 'まちがい。答えはもっと小さいですよ';
+            re = document.querySelector('div#result');
+            re.insertAdjacentElement('afterend', p);
             console.log("まちがい。答えはもっと小さいですよ");
         }
         else {
+            p.textContent = 'まちがい。答えはもっと大きいですよ';
+            re = document.querySelector('div#result');
+            re.insertAdjacentElement('afterend', p);
             console.log("まちがい。答えはもっと大きいですよ");
         }
     }
     else {
+        p.textContent = '答えは " + kotae + " でした。すでにゲームは終わっています';
+        re = document.querySelector('div#result');
+        re.insertAdjacentElement('afterend', p);
         console.log("答えは " + kotae + " でした。すでにゲームは終わっています");
     }
 
